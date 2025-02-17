f635b7bec8f4de535f2a15d924925d19c3f57b4a
Nkosi <masangonim@gmail.com>
2025-02-17 06:12:00 +0200

    modified files

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,9 +1,11 @@
 import Card from "@/components/Card"
+import Button from "@/component/Button"
 const Landing: React.FC =  () => {
     return (
       <div>
-        <h1 className=" text-xl font-extralight">Landing Page</h1>
+        <h1 className=" text-xl font-extralight rounded-sm">Landing Page</h1>
         <Card />
+        <Button title={""} />
       </div>
     )
   }
